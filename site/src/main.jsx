d6842fa3079b3d0473d8d59d6b090255622b1144
Brayden <coghill30@gmail.com>
2025-03-26 11:15:20 -0700

    Updating files after PR

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,5 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Navbar from './components/layout/Navbar/Navbar.jsx'
@@ -7,12 +7,12 @@ import ApiTestBlock from "./components/features/ApiTestBlock.jsx";
 import OddsViewer from './components/features/OddsViewer';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <Navbar />
-      <div>
-          This is a test component
-          {/* <ApiTestBlock /> */}
-            <OddsViewer />
-      </div>
-  </StrictMode>,
+    <StrictMode>
+        <Navbar/>
+        <div>
+            This is a test component
+            {/* <ApiTestBlock /> */}
+            <OddsViewer/>
+        </div>
+    </StrictMode>,
 )
